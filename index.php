fad0d43af4fae3bb1691028635a5a51edcbd2977
Daniele Moro <moro.daniele@gmail.com>
2018-09-24 23:42:09 -0600

    about page

--- index.php
+++ index.php
@@ -1,2 +1,27 @@
-<?php include_once("index.html"); ?>
 <?php include_once("header.html"); ?>
+
+<html>
+    <header>
+        <title>Peeps</title>
+
+        <!-- Roboto font from Google -->
+        <link href='https://fonts.googleapis.com/css?family=Roboto+Condensed:700' rel='stylesheet' type='text/css'>
+        <link href='https://fonts.googleapis.com/css?family=Roboto:100' rel='stylesheet' type='text/css'>
+        <link href="headerstyle.css" type="text/css" rel="stylesheet" />
+        <link href="style.css" type="text/css" rel="stylesheet" />
+    </header>
+    <body>
+        <?php include_once("header.html"); ?>
+        <h1> <img src="media/logo" id="logoTitle">Peeps</h1>
+        <h4> Have you ever forgotten someone's name or other information? Look no further </h4>
+        <p> Every day we meet many people and we re-encounter others we have met before. However, many people have
+        difficulty remembering a person’s name, occupation, school, and other attributes. Many of us sometimes find
+        ourselves talking to someone who we know we’ve met before, but we don’t remember anything about them. </p>
+        <p> Peeps solves this problem by allowing you to create and modify contacts through a fast and intuitive natural
+         language system in the form of a chat bot. </p>
+        <div class="buttonRow">
+            <button>Log In</button>
+            <button class="buttonHot">Sign Up</button>
+        </div>
+    </body>
+</html>
