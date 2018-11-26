541013f2c75a59cdbdfd236334c7820a0df95796
Daniele Moro <moro.daniele@gmail.com>
2018-11-25 17:42:51 -0700

    added more info in the about page

--- index.php
+++ index.php
@@ -16,6 +16,9 @@
             ourselves talking to someone who we know we’ve met before, but we don’t remember anything about them. </p>
             <p> Peeps solves this problem by allowing you to create and modify contacts through a fast and intuitive natural
              language system in the form of a chatbot. </p>
+            <p> This project takes huge inspiration from <a href="https://helloaugi.com">Augi</a> and <a href="https://todoist.com/">Todoist</a>,
+              so please check them out. Peeps is a work-in-progress, and it is meant to serve as a learning exercise for the author.
+               Peeps was made by <a href="https://danielemoro.com">Daniele Moro</a> in the Fall of 2018. Here is the <a href="https://github.com/danielemoro/peeps">source code</a>.</p>
             <?php if(!isset($_SESSION['logged_in']) or !$_SESSION['logged_in']) { ?>
             <div class="buttonRow">
                 <button onclick="location.href='login.php'">Log In</button>
